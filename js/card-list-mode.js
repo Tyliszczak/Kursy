32a872fc20022c6651708490317a41810b332c10
Tyliszczak <kswiderski.de@gmail.com>
2026-08-13 12:18:40 +0200

    Show correct test version 0.7.5

--- js/card-list-mode.js
+++ js/card-list-mode.js
@@ -82,5 +82,5 @@
     }
   });
 
-  requestAnimationFrame(()=>{const b=document.querySelector('.badge');if(b)b.textContent='WERSJA TESTOWA 0.7.3'});
+  requestAnimationFrame(()=>{const b=document.querySelector('.badge');if(b)b.textContent='WERSJA TESTOWA 0.7.5'});
 })();
