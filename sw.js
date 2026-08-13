a0e7f7c8dc3331633670d4c327af2c372788199b
Tyliszczak <kswiderski.de@gmail.com>
2026-08-13 10:28:22 +0200

    Odśwież cache dla stabilnej wersji 0.6.0

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='kursy-v0.5.2';
-const APP_ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./css/app.css','./js/app.js','./js/data.js','./js/api.js','./js/map-editor-bridge.js','./js/ui-fixes.js','./js/enhancements-052.js','./map-editor.html'];
+const CACHE='kursy-v0.6.0';
+const APP_ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./css/app.css','./js/app-v2.js','./js/data.js','./js/api.js','./js/map-editor-bridge.js','./map-editor.html'];
 const DATA_ASSETS=['./data/company.json','./data/routes.json','./data/courses.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll([...APP_ASSETS,...DATA_ASSETS])).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
