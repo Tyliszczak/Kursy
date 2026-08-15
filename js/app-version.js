261004f3e0a8cc993f97b7cf77318caff1c005fe
Tyliszczak <kswiderski.de@gmail.com>
2026-08-15 10:27:54 +0200

    Dodaj odzyskiwanie hasła właściciela: wersja 0.12.9

--- js/app-version.js
+++ js/app-version.js
@@ -1,5 +1,5 @@
 (()=>{
-  const VERSION='0.12.8';
+  const VERSION='0.12.9';
   globalThis.KURSY_APP_VERSION=VERSION;
   const paint=()=>{const b=document.querySelector('.badge');if(b)b.textContent=`WERSJA TESTOWA ${VERSION}`};
   if(typeof document!=='undefined'){
