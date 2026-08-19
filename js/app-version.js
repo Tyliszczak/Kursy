5ccf9004ff1429e8d0ff014e5d9c025ac164c340
Tyliszczak <kswiderski.de@gmail.com>
2026-08-19 11:22:28 +0200

    Odśwież PWA po zmianie ikon

--- js/app-version.js
+++ js/app-version.js
@@ -1,5 +1,5 @@
 (()=>{
-  const VERSION='0.13.1';
+  const VERSION='0.13.2';
   globalThis.KURSY_APP_VERSION=VERSION;
   const paint=()=>{const b=document.querySelector('.badge');if(b)b.textContent=`WERSJA TESTOWA ${VERSION}`};
   if(typeof document!=='undefined'){
